a460766e7166a12efea281162302ca4cf4eb5b50
Reinhard Prix <reinhard.prix@aei.mpg.de>
2024-01-08 16:46:26 +0100

    provide adapted 'Android.mk' build file for libpng-1.6.25

--- Android.mk
+++ Android.mk
@@ -1,40 +1,31 @@
-LOCAL_PATH:= $(call my-dir)
-
+# Auto-generated module by script
+LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-common_SRC_FILES := \
-    png.c pngerror.c \
-	pngget.c pngmem.c pngpread.c pngread.c pngrio.c pngrtran.c pngrutil.c \
-	pngset.c pngtrans.c pngwio.c pngwrite.c pngwtran.c pngwutil.c
-
-ifeq ($(TARGET_ARCH_ABI),armeabi)
-common_SRC_FILES += \
-	arm/arm_init.c arm/filter_neon.S arm/filter_neon_intrinsics.c arm/palette_neon_intrinsics.c
-endif
-ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-common_SRC_FILES += \
-	arm/arm_init.c arm/filter_neon.S arm/filter_neon_intrinsics.c arm/palette_neon_intrinsics.c
-endif
-ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-common_SRC_FILES += \
-	arm/arm_init.c arm/filter_neon.S arm/filter_neon_intrinsics.c arm/palette_neon_intrinsics.c
-endif
-
-common_CFLAGS := ## -fomit-frame-pointer
-
-common_C_INCLUDES += \
-
-# For the host
-# =====================================================
-
-LOCAL_SRC_FILES := $(common_SRC_FILES)
-LOCAL_CFLAGS += $(common_CFLAGS)
-LOCAL_C_INCLUDES += $(common_C_INCLUDES)
-
-LOCAL_MODULE:= png
-
-LOCAL_COPY_HEADERS_TO := $(common_COPY_HEADERS_TO)
-LOCAL_COPY_HEADERS := $(common_COPY_HEADERS)
+LOCAL_MODULE := libpng
+LOCAL_C_INCLUDES := 
+LOCAL_CFLAGS :=
+LOCAL_CPPFLAGS := 
+LOCAL_LDLIBS := 
+LOCAL_PREBUILTS := 
+LOCAL_SRC_FILES := ./png.c \
+./pngerror.c \
+./pngget.c \
+./pngmem.c \
+./pngpread.c \
+./pngread.c \
+./pngrio.c \
+./pngrtran.c \
+./pngrutil.c \
+./pngset.c \
+./pngtrans.c \
+./pngwio.c \
+./pngwrite.c \
+./pngwtran.c \
+./pngwutil.c \
+./arm/arm_init.c \
+./arm/filter_neon.S \
+./arm/filter_neon_intrinsics.c
 
 include $(BUILD_STATIC_LIBRARY)
 #include $(BUILD_SHARED_LIBRARY)
